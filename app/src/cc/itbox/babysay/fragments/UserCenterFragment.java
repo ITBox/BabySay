7159a3614e3a404a45b3e8d2ed26a7d6479c0b93
baoyongzhang <baoyz@live.com>
2014-02-23 13:47:27 +0800

    test

--- app/src/cc/itbox/babysay/fragments/UserCenterFragment.java
+++ app/src/cc/itbox/babysay/fragments/UserCenterFragment.java
@@ -2,6 +2,8 @@ package cc.itbox.babysay.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import cc.itbox.babysay.R;
@@ -10,13 +12,12 @@ import cc.itbox.babysay.activities.MainActivity.BaseFragment;
 /**
  * 用户中心页面
  * 
- * @author baoyz 
+ * @author baoyz
  * 
- * 2014-2-22 下午10:42:28
- *
+ *         2014-2-22 下午10:42:28
+ * 
  */
 public class UserCenterFragment extends BaseFragment {
-
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,5 +29,12 @@ public class UserCenterFragment extends BaseFragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_guide, container, false);
 		return view;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// 创建显示导航菜单时的Actionbar菜单
+		inflater.inflate(R.menu.main_page, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 }
