b5e8a771e3fb705d492b682b658ea424a5e60701
malinkang <malinkang857@gmail.com>
2014-02-23 12:50:23 +0800

    update

--- app/src/cc/itbox/babysay/fragments/RegisterFragment.java
+++ app/src/cc/itbox/babysay/fragments/RegisterFragment.java
@@ -3,16 +3,16 @@ package cc.itbox.babysay.fragments;
 /**
  * 注册
  */
-import cc.itbox.babysay.R;
-import cc.itbox.babysay.ui.FloatLabel;
-import cc.itbox.babysay.ui.FloatLabelAnimator;
-import cc.itbox.babysay.util.UIUtil;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import cc.itbox.babysay.R;
+import cc.itbox.babysay.ui.FloatLabel;
+import cc.itbox.babysay.ui.FloatLabelAnimator;
+import cc.itbox.babysay.util.UIUtil;
 
 public class RegisterFragment extends BaseFragment {
 
@@ -20,20 +20,23 @@ public class RegisterFragment extends BaseFragment {
 
 	@Override
 	protected void onHandleMessage(Message message) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		View layout = null;
 		if (container != null) {
-			LayoutInflater layoutInflater = (LayoutInflater) mActThis.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			layout = layoutInflater.inflate(R.layout.fragment_register, container, false);
-			mETRegName = UIUtil.getView(mActThis, R.id.fragment_register_name);
-			mETRegEmail = UIUtil.getView(mActThis, R.id.fragment_register_email);
-			mETRegPass = UIUtil.getView(mActThis, R.id.fragment_register_pass);
-			mETRegPassAgain = UIUtil.getView(mActThis, R.id.fragment_register_pass_again);
+			LayoutInflater layoutInflater = (LayoutInflater) mActThis
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			layout = layoutInflater.inflate(R.layout.fragment_register,
+					container, false);
+			mETRegName = UIUtil.getView(layout, R.id.fragment_register_name);
+			mETRegEmail = UIUtil.getView(layout, R.id.fragment_register_email);
+			mETRegPass = UIUtil.getView(layout, R.id.fragment_register_pass);
+			mETRegPassAgain = UIUtil.getView(layout,
+					R.id.fragment_register_pass_again);
 			mETRegName.setLabelAnimator(new FloatLabelAnimator());
 			mETRegEmail.setLabelAnimator(new FloatLabelAnimator());
 			mETRegPass.setLabelAnimator(new FloatLabelAnimator());
