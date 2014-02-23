4bcb1985b81703ad5f3facf7bd939508e0fbb391
baoyongzhang <baoyz@live.com>
2014-02-23 17:21:50 +0800

    ActionBar下拉刷新库

---
+++ app/src/cc/itbox/babysay/adapter/DMListAdapter.java
@@ -0,0 +1,41 @@
+package cc.itbox.babysay.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 动态信息列表适配器
+ * 
+ * @author baoyz 
+ * 
+ * 2014-2-23 下午4:42:54
+ *
+ */
+public class DMListAdapter extends BaseAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ app/src/cc/itbox/babysay/bean/DMBean.java
@@ -0,0 +1,13 @@
+package cc.itbox.babysay.bean;
+
+/**
+ * 动态信息数据实体
+ * 
+ * @author baoyz
+ * 
+ *         2014-2-23 下午4:44:28
+ * 
+ */
+public class DMBean {
+
+}
