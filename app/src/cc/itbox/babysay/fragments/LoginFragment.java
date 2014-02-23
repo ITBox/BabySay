98f2c27f5d47efc68a5489b30907bedc3a8d1ff5
yanchenling <yclwangyi@163.com>
2014-02-23 11:42:23 +0800

    login

---
+++ app/src/cc/itbox/babysay/fragments/LoginFragment.java
@@ -0,0 +1,20 @@
+package cc.itbox.babysay.fragments;
+
+import cc.itbox.babysay.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View view = inflater.inflate(R.layout.fragment_login, container, false);
+		
+		return view;
+	}
+}
