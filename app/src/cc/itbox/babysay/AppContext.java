76fab9525a7a0950701755c9d66a65da7b1df2ee
baoyongzhang <baoyz@live.com>
2014-03-02 18:42:49 +0800

    主页面动态信息列表

--- app/src/cc/itbox/babysay/AppContext.java
+++ app/src/cc/itbox/babysay/AppContext.java
@@ -2,6 +2,12 @@ package cc.itbox.babysay;
 
 import org.holoeverywhere.app.Application;
 
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 public class AppContext extends Application {
 	private static AppContext mContext;
 
@@ -13,5 +19,19 @@ public class AppContext extends Application {
 	public void onCreate() {
 		super.onCreate();
 		mContext = this;
+
+		// 初始化图片加载器
+		DisplayImageOptions options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.aaa)
+				.showImageForEmptyUri(R.drawable.aaa)
+				.showImageOnFail(R.drawable.aaa).cacheInMemory(true)
+				.cacheOnDisc(true).build();
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+				this).threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())
+				.defaultDisplayImageOptions(options).writeDebugLogs()
+				.tasksProcessingOrder(QueueProcessingType.LIFO).build();
+		ImageLoader.getInstance().init(config);
 	}
 }
