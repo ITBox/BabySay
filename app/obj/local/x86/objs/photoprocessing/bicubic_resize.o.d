f1122cb06ef33ff1eb9201f8b35736ad2eb66a0d
baoyongzhang <baoyz@live.com>
2014-03-12 22:20:24 +0800

    编译滤镜库x86版本

---
+++ app/obj/local/x86/objs/photoprocessing/bicubic_resize.o.d
@@ -0,0 +1,74 @@
+/Users/baoyz/GitHub/Repositories/BabySay/app/obj/local/x86/objs/photoprocessing/bicubic_resize.o: \
+ /Users/baoyz/GitHub/Repositories/BabySay/app/jni/bicubic_resize.c \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/stdlib.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/cdefs.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/android/api-level.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/string.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/malloc.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/alloca.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/strings.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/stdint.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/_types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/machine/_types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/posix_types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/stddef.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/compiler.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/posix_types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/types.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/machine/kernel.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/memory.h \
+ /Users/baoyz/GitHub/Repositories/BabySay/app/jni/mem_utils.h
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/stdlib.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/android/api-level.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/string.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/malloc.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/alloca.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/strings.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/stdint.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/_types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/machine/_types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/stddef.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/compiler.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/asm/types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/linux/types.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/machine/kernel.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/baoyz/Developer/Android/android-ndk-r9c/platforms/android-19/arch-x86/usr/include/memory.h:
+
+/Users/baoyz/GitHub/Repositories/BabySay/app/jni/mem_utils.h:
